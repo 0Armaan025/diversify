69b3e53035ed0634e014aed6a368a3513ee30ae5
0Armaan025 <armaan33000@gmail.com>
2023-02-25 07:32:29 +0530

    getting bored

--- lib/common/widgets/post_widget.dart
+++ lib/common/widgets/post_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PostWidget extends StatelessWidget {
   const PostWidget({super.key});
@@ -10,7 +11,7 @@ class PostWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18.0),
       child: Container(
-        height: size.height * 0.35,
+        height: size.height * 0.43,
         width: double.infinity,
         decoration: BoxDecoration(
           color: Colors.lightGreen[600],
@@ -26,7 +27,8 @@ class PostWidget extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(3.0),
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(''),
+                      backgroundImage: NetworkImage(
+                          'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'),
                     ),
                   ),
                   Column(
@@ -35,11 +37,23 @@ class PostWidget extends StatelessWidget {
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(left: 13.0),
-                        child: Text("Armaan"),
+                        child: Text(
+                          "Armaan",
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
                       ),
                       Padding(
                         padding: const EdgeInsets.only(left: 13.0),
-                        child: Text("armaan33000@gmail.com"),
+                        child: Text(
+                          "armaan33000@gmail.com",
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
                       ),
                     ],
                   ),
@@ -53,15 +67,44 @@ class PostWidget extends StatelessWidget {
             ),
             Row(
               children: [
-                Container(
-                  width: size.width * 0.5,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image:
-                          NetworkImage('https://via.placeholder.com/350x150'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: size.width * 0.45,
+                    height: size.height * 0.24,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
+                ),
+                Container(
+                  width: size.width * 0.35,
+                  padding: const EdgeInsets.all(8),
+                  height: size.height * 0.24,
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreenAccent[400],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    "Cillum laboris reprehenderit labore in deserunt aliqua veniam laborum sint amet exercitation sit. Aute est nostrud consectetur...",
+                    style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Chip(
+                  label: Text('India'),
                 ),
               ],
             ),
